dde540bc8a442682a805087b5229ac7bce006901
saifiimuhammad <muhammadsaifarain786@gmail.com>
2025-01-13 16:04:29 +0500

    Fixed cors issue

--- server/constants/config.js
+++ server/constants/config.js
@@ -4,6 +4,7 @@ const corsOptions = {
     "http://localhost:4173",
     "http://172.22.96.1:5173",
     "http://192.168.0.103:5173",
+    "*",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
